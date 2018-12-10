eebfc081280f798917beb109b177d6fa7bdfea86
S. McGuire <smcguire36@gmail.com>
2018-12-10 12:47:19 -0500

    Initial project template build.

---
+++ src/java/ConfigurationBean.java
@@ -0,0 +1,15 @@
+
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+/**
+ *
+ * @author mcguire
+ */
+@FacesConfig(
+// Activates CDI built-in beans
+version = JSF_2_3 
+)
+public class ConfigurationBean {
+
+}
